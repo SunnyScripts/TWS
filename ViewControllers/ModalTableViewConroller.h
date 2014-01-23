7f158f765c154715cb73c18a5aa682e998941552
Ryan Berg <rberg2@hotmail.com>
2014-01-23 14:31:49 -0800

    Create ModalTableViewConroller.h

---
+++ ViewControllers/ModalTableViewConroller.h
@@ -0,0 +1,17 @@
+//
+//  modalTableViewViewController.h
+//  ThisWifiSucks
+//
+//  Created by Ryan Berg on 12/25/13.
+//  Copyright (c) 2013 Ryan Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalTableViewViewController : UIViewController
+
+- (IBAction)cancel:(id)sender;
+- (IBAction)poweredByFS:(id)sender;
+
+
+@end
