f127f908fab9183ebb776573677d294a04a8a6de
Ryan Berg <rberg2@hotmail.com>
2014-01-23 14:33:24 -0800

    Create InfoViewController.h

---
+++ ViewControllers/InfoViewController.h
@@ -0,0 +1,14 @@
+//
+//  InfoViewController.h
+//  ThisWifiSucks
+//
+//  Created by Ryan Berg on 12/15/13.
+//  Copyright (c) 2013 Ryan Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+- (IBAction)backButtonClicked:(id)sender;
+
+@end
