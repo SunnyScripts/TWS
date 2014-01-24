2cca920ddf3a4f628e86113cc55ece0673a2cc1b
Ryan Berg <rberg2@hotmail.com>
2014-01-23 16:51:51 -0800

    Create GraphView.h

---
+++ OverriddenClasses/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  ThisWifiSucks
+//
+//  Created by Ryan Berg on 12/15/13.
+//  Copyright (c) 2013 Ryan Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
